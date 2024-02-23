2fa3e52af4e2c2174c0859572547d0d48cd42735
aclementem <aclementem91@gmail.com>
2024-02-23 22:59:47 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1 className="text-3xl font-extrabold text-gray-600 text-center">
+      ¿Cuánto <span className="text-indigo-500"> dinero </span>necesitas?
+    </h1>
+  );
+}
+
+export default Header;
